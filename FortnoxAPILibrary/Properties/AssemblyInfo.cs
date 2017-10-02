ad162272005bdc73bed286d7a2297a6ae8663b9f
David Ulvmoen <david@it-integration.se>
2017-10-02 15:22:36 +0200

    Update version

--- FortnoxAPILibrary/Properties/AssemblyInfo.cs
+++ FortnoxAPILibrary/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fortnox AB (publ)")]
 [assembly: AssemblyProduct("FortnoxAPILibrary")]
-[assembly: AssemblyCopyright("Copyright © Fortnox 2013")]
+[assembly: AssemblyCopyright("Copyright © Fortnox 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.1.6")]
