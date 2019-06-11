8889943163172bf8e156e3c7804a095fbd003e36
Artur Brutian <artur.brutian@zwapgrid.com>
2019-06-11 10:18:42 +0000

    Add custom TooManyRequests exception

---
+++ FortnoxAPILibrary/Exceptions/FortnoxTooManyRequestsException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace FortnoxAPILibrary.Exceptions
+{
+    public class FortnoxTooManyRequestsException : Exception
+    {
+        public HttpStatusCode Code { get; set; }
+
+        public FortnoxTooManyRequestsException()
+        {
+        }
+
+        public FortnoxTooManyRequestsException(string message) : base(message)
+        {
+        }
+
+        public FortnoxTooManyRequestsException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
