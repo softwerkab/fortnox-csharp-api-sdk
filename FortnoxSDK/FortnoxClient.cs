918b85515b94f53826b7a9f6abd4636d4da9baeb
Richard Randak <richard.randak@softwerk.se>
2021-06-07 11:17:52 +0200

    Add predefined connectors to FortnoxClient

--- FortnoxSDK/FortnoxClient.cs
+++ FortnoxSDK/FortnoxClient.cs
@@ -1,5 +1,8 @@
-﻿using Fortnox.SDK.Connectors.Base;
+﻿using System;
+using Fortnox.SDK.Connectors.Base;
 using System.Net.Http;
+using Fortnox.SDK.Connectors;
+using Fortnox.SDK.Interfaces;
 
 namespace Fortnox.SDK
 {
@@ -39,6 +42,7 @@ namespace Fortnox.SDK
         /// </summary>
         /// <typeparam name="TConnector">Connector type to be created. In future, interface type may be used instead</typeparam>
         /// <returns></returns>
+        [Obsolete("You can now get the connector through a predefined property. Example: 'FortnoxClient.CustomerConnector' instead of 'FortnoxClient.Get<CustomerConnector>()'")]
         public TConnector Get<TConnector>() where TConnector : BaseConnector, new()
         {
             return new TConnector()
@@ -49,5 +53,63 @@ namespace Fortnox.SDK
                 HttpClient = HttpClient
             };
         }
+
+        public IAbsenceTransactionConnector AbsenceTransactionConnector => Get<AbsenceTransactionConnector>();
+        public IAccountChartConnector AccountChartConnector => Get<AccountChartConnector>();
+        public IAccountConnector AccountConnector => Get<AccountConnector>();
+        public IArchiveConnector ArchiveConnector => Get<ArchiveConnector>();
+        public IArticleConnector ArticleConnector => Get<ArticleConnector>();
+        public IArticleFileConnectionConnector ArticleFileConnectionConnector => Get<ArticleFileConnectionConnector>();
+        public IAssetConnector AssetConnector => Get<AssetConnector>();
+        public IAssetFileConnectionConnector AssetFileConnectionConnector => Get<AssetFileConnectionConnector>();
+        public IAssetTypesConnector AssetTypesConnector => Get<AssetTypesConnector>();
+        public IAttendanceTransactionsConnector AttendanceTransactionsConnector => Get<AttendanceTransactionsConnector>();
+        public IAuthorizationConnector AuthorizationConnector => Get<AuthorizationConnector>();
+        public ICompanyInformationConnector CompanyInformationConnector => Get<CompanyInformationConnector>();
+        public ICompanySettingsConnector CompanySettingsConnector => Get<CompanySettingsConnector>();
+        public IContractAccrualConnector ContractAccrualConnector => Get<ContractAccrualConnector>();
+        public IContractConnector ContractConnector => Get<ContractConnector>();
+        public IContractTemplateConnector ContractTemplateConnector => Get<ContractTemplateConnector>();
+        public ICostCenterConnector CostCenterConnector => Get<CostCenterConnector>();
+        public ICurrencyConnector CurrencyConnector => Get<CurrencyConnector>();
+        public ICustomerConnector CustomerConnector => Get<CustomerConnector>();
+        public IEmployeeConnector EmployeeConnector => Get<EmployeeConnector>();
+        public IExpenseConnector ExpenseConnector => Get<ExpenseConnector>();
+        public IFinancialYearConnector FinancialYearConnector => Get<FinancialYearConnector>();
+        public IArchiveConnector InboxConnector => Get<InboxConnector>(); //Note: Uses same interface as ArchiveConnector
+        public IInvoiceAccrualConnector InvoiceAccrualConnector => Get<InvoiceAccrualConnector>();
+        public IInvoiceConnector InvoiceConnector => Get<InvoiceConnector>();
+        public IInvoiceFileConnectionConnector InvoiceFileConnectionConnector => Get<InvoiceFileConnectionConnector>();
+        public IInvoicePaymentConnector InvoicePaymentConnector => Get<InvoicePaymentConnector>();
+        public ILabelConnector LabelConnector => Get<LabelConnector>();
+        public ILockedPeriodConnector LockedPeriodConnector => Get<LockedPeriodConnector>();
+        public IModeOfPaymentConnector ModeOfPaymentConnector => Get<ModeOfPaymentConnector>();
+        public IOfferConnector OfferConnector => Get<OfferConnector>();
+        public IOrderConnector OrderConnector => Get<OrderConnector>();
+        public IPredefinedAccountsConnector PredefinedAccountsConnector => Get<PredefinedAccountsConnector>();
+        public IPredefinedVoucherSeriesConnector PredefinedVoucherSeriesConnector => Get<PredefinedVoucherSeriesConnector>();
+        public IPriceConnector PriceConnector => Get<PriceConnector>();
+        public IPriceListConnector PriceListConnector => Get<PriceListConnector>();
+        public IPrintTemplateConnector PrintTemplateConnector => Get<PrintTemplateConnector>();
+        public IProjectConnector ProjectConnector => Get<ProjectConnector>();
+        public ISalaryTransactionConnector SalaryTransactionConnector => Get<SalaryTransactionConnector>();
+        public IScheduleTimesConnector ScheduleTimesConnector => Get<ScheduleTimesConnector>();
+        public ISIEConnector SIEConnector => Get<SIEConnector>();
+        public ISupplierConnector SupplierConnector => Get<SupplierConnector>();
+        public ISupplierInvoiceAccrualConnector SupplierInvoiceAccrualConnector => Get<SupplierInvoiceAccrualConnector>();
+        public ISupplierInvoiceConnector SupplierInvoiceConnector => Get<SupplierInvoiceConnector>();
+        public ISupplierInvoiceExternalURLConnectionConnector SupplierInvoiceExternalURLConnectionConnector => Get<SupplierInvoiceExternalURLConnectionConnector>();
+        public ISupplierInvoiceFileConnectionConnector SupplierInvoiceFileConnectionConnector => Get<SupplierInvoiceFileConnectionConnector>();
+        public ISupplierInvoicePaymentConnector SupplierInvoicePaymentConnector => Get<SupplierInvoicePaymentConnector>();
+        public ITaxReductionConnector TaxReductionConnector => Get<TaxReductionConnector>();
+        public ITermsOfDeliveryConnector TermsOfDeliveryConnector => Get<TermsOfDeliveryConnector>();
+        public ITermsOfPaymentConnector TermsOfPaymentConnector => Get<TermsOfPaymentConnector>();
+        public ITrustedEmailDomainsConnector TrustedEmailDomainsConnector => Get<TrustedEmailDomainsConnector>();
+        public ITrustedEmailSendersConnector TrustedEmailSendersConnector => Get<TrustedEmailSendersConnector>();
+        public IUnitConnector UnitConnector => Get<UnitConnector>();
+        public IVoucherConnector VoucherConnector => Get<VoucherConnector>();
+        public IVoucherFileConnectionConnector VoucherFileConnectionConnector => Get<VoucherFileConnectionConnector>();
+        public IVoucherSeriesConnector VoucherSeriesConnector => Get<VoucherSeriesConnector>();
+        public IWayOfDeliveryConnector WayOfDeliveryConnector => Get<WayOfDeliveryConnector>();
     }
 }
