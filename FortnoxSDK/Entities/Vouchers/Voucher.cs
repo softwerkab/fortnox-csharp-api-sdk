da0c60c979cf40262625f64927377e09224dffb7
Jonathan <vanillajonathan@users.noreply.github.com>
2021-06-21 13:50:00 +0200

    Initialize the collection
    https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/guidelines-for-collections

--- FortnoxSDK/Entities/Vouchers/Voucher.cs
+++ FortnoxSDK/Entities/Vouchers/Voucher.cs
@@ -8,61 +8,60 @@ namespace Fortnox.SDK.Entities
     [Entity(SingularName = "Voucher", PluralName = "Vouchers")]
     public class Voucher
     {
-
-        ///<summary> Direct URL to the record. </summary>
+        /// <summary>Direct URL to the record.</summary>
         [ReadOnly]
         [JsonProperty("@url")]
         public string Url { get; private set; }
 
-        ///<summary> Comments of the voucher. </summary>
+        /// <summary>Comments of the voucher.</summary>
         [JsonProperty]
         public string Comments { get; set; }
 
-        ///<summary> Code of the cost center. The code must be of an existing cost center. </summary>
+        /// <summary>Code of the cost center. The code must be of an existing cost center.</summary>
         [JsonProperty]
         public string CostCenter { get; set; }
 
-        ///<summary> Description of the voucher. </summary>
+        /// <summary>Description of the voucher.</summary>
         [JsonProperty]
         public string Description { get; set; }
 
-        ///<summary> Code of the project. The code must be of an existing project. </summary>
+        /// <summary>Code of the project. The code must be of an existing project.</summary>
         [JsonProperty]
         public string Project { get; set; }
 
-        ///<summary> Reference number, for example an invoice number. </summary>
+        /// <summary>Reference number, for example an invoice number.</summary>
         [ReadOnly]
         [JsonProperty]
         public string ReferenceNumber { get; private set; }
 
-        ///<summary> Reference type. Can be INVOICE SUPPLIERINVOICE INVOICEPAYMENT SUPPLIERPAYMENT MANUAL CASHINVOICE or ACCRUAL </summary>
+        /// <summary>Reference type. Can be INVOICE SUPPLIERINVOICE INVOICEPAYMENT SUPPLIERPAYMENT MANUAL CASHINVOICE or ACCRUAL</summary>
         [ReadOnly]
         [JsonProperty]
         public ReferenceType? ReferenceType { get; private set; }
 
-        ///<summary> Date of the transaction. Must be a valid date according to our date format. </summary>
+        /// <summary>Date of the transaction. Must be a valid date according to our date format.</summary>
         [JsonProperty]
         public DateTime? TransactionDate { get; set; }
 
-        ///<summary> Number of the voucher </summary>
+        /// <summary>Number of the voucher</summary>
         [ReadOnly]
         [JsonProperty]
         public long? VoucherNumber { get; private set; }
 
-        ///<summary> The properties for the object in this array is listed in the table for “Voucher Rows”. </summary>
+        /// <summary>The properties for the object in this array is listed in the table for “Voucher Rows”.</summary>
         [JsonProperty]
-        public List<VoucherRow> VoucherRows { get; set; }
+        public ICollection<VoucherRow> VoucherRows { get; set; } = new List<VoucherRow>();
 
-        ///<summary> Code of the voucher series. The code must be of an existing voucher series. </summary>
+        /// <summary>Code of the voucher series. The code must be of an existing voucher series.</summary>
         [JsonProperty]
         public string VoucherSeries { get; set; }
 
-        ///<summary> Id of the year of the voucher. </summary>
+        /// <summary>Id of the year of the voucher.</summary>
         [ReadOnly]
         [JsonProperty]
         public long? Year { get; private set; }
 
-        ///<summary> The approval state f the voucher.  Not for approval: 0  Not ready for approval: 1  Not approved: 2  Approved: 3 </summary>
+        /// <summary>The approval state f the voucher.  Not for approval: 0  Not ready for approval: 1  Not approved: 2  Approved: 3</summary>
         [ReadOnly]
         [JsonProperty]
         public long? ApprovalState { get; private set; }
