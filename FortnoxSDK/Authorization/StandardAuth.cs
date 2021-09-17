c1736da4122a6dbd7b2e3fd311ff42304f641749
Jonathan <vanillajonathan@users.noreply.github.com>
2021-09-17 11:23:11 +0200

    Add argument check
    Add guard to check the method argument.

--- FortnoxSDK/Authorization/StandardAuth.cs
+++ FortnoxSDK/Authorization/StandardAuth.cs
@@ -6,10 +6,14 @@ namespace Fortnox.SDK.Authorization
     /// Handles authorization by standard JWT token obtained by OAuth2 workflow.
     /// </summary>
     public class StandardAuth : FortnoxAuthorization
-
     {
         public StandardAuth(string accessToken)
         {
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new ArgumentException("The access token is null or empty.", nameof(accessToken));
+            }
+
             AccessToken = accessToken;
         }
 
