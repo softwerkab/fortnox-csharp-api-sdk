69aa2a902dcc3b12a1bb2b3c451b8108cf409794
Jonathan <vanillajonathan@users.noreply.github.com>
2021-10-15 16:51:27 +0200

    Add documentation

--- FortnoxSDK/Auth/TokenInfo.cs
+++ FortnoxSDK/Auth/TokenInfo.cs
@@ -4,18 +4,30 @@ namespace Fortnox.SDK.Auth
 {
     public class TokenInfo
     {
+        /// <summary>
+        /// The access token issued by the authorization server.
+        /// </summary>
         [JsonProperty("access_token")]
         public string AccessToken { get; set; }
 
+        /// <summary>
+        /// The refresh token, which can be used to obtain new access tokens using the same authorization grant.
+        /// </summary>
         [JsonProperty("refresh_token")]
         public string RefreshToken { get; set; }
 
         [JsonProperty("scope")]
         public string Scope { get; set; }
 
+        /// <summary>
+        /// The lifetime in seconds of the access token.
+        /// </summary>
         [JsonProperty("expires_in")]
         public int ExpiresIn { get; set; }
 
+        /// <summary>
+        /// The type of the token issued.
+        /// </summary>
         [JsonProperty("token_type")]
         public string TokenType { get; set; }
     }
