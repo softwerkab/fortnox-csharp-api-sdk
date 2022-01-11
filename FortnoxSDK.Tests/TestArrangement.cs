b1efcec6e03aa6a0b8049e9c4b61b345cf3fb67e
Richard Randak <richard.randak@softwerk.se>
2022-01-11 14:30:43 +0100

    Add workaround for corrupted test state

---
+++ FortnoxSDK.Tests/TestArrangement.cs
@@ -0,0 +1,25 @@
+﻿using Fortnox.SDK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace FortnoxSDK.Tests;
+
+[TestClass]
+public class TestArrangement
+{
+    public static FortnoxClient FortnoxClient = TestUtils.DefaultFortnoxClient;
+
+    [AssemblyInitialize]
+    public static async Task ArrangeSandbox(TestContext context)
+    {
+        try
+        {
+            // Delete hanging entities from failed execution
+            await FortnoxClient.CostCenterConnector.DeleteAsync("TMP");
+        }
+        catch
+        {
+            // Ignore
+        }
+    }
+}
