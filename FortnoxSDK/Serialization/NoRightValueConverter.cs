88b872b5e66816cfd786b991133c8ff5dfd5dce3
Jakob Hägg <98894053+JakobHaggCAB@users.noreply.github.com>
2023-03-17 09:24:32 +0100

    Added no-right-value-converter to parse Fortnox API_NORIGHT values
    This solves issues (ex #263)  when fortnox sends magic string for values that isn't strings.

---
+++ FortnoxSDK/Serialization/NoRightValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fortnox.SDK.Serialization
+{
+    /// <summary>
+    /// Fortnox use a magic string value "API_NORIGHT" for when customer is missing a license to features.
+    /// All those properties are nullable and we can simply parse those values as null.
+    /// </summary>
+    public class NoRightValueConverter : JsonConverter
+    {
+        readonly JsonSerializer defaultSerializer = new JsonSerializer();
+
+        public override bool CanConvert(Type objectType)
+            => Nullable.GetUnderlyingType(objectType) != null;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            => reader.TokenType == JsonToken.String && (string)reader.Value == "API_NORIGHT" ?
+                null : defaultSerializer.Deserialize(reader, objectType);
+
+        public override bool CanWrite 
+            => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            => throw new NotImplementedException();
+    }
+}
